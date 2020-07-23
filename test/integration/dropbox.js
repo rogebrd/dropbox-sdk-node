2b41ca6fe3cbd14cbe7f63136ae6145c251c3469
Andrew Lawson <alawson@dropbox.com>
2020-07-23 13:34:24 -0700

    Add download and upload tests. Other small changes.

--- test/integration/dropbox.js
+++ test/integration/dropbox.js
@@ -1,9 +1,12 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import chai from 'chai';
 
 import Dropbox from '../../index.js';
 import { DropboxResponse } from '../../src/response.js';
 
-describe('Dropbox', () => {
+describe('User', () => {
   const dbx = new Dropbox({ accessToken: process.env.DBX_ACCESS_TOKEN });
 
   describe('rpc', () => {
@@ -11,11 +14,46 @@ describe('Dropbox', () => {
       dbx.usersGetCurrentAccount()
         .then((resp) => {
           chai.assert.instanceOf(resp, DropboxResponse);
+          chai.assert.equal(resp.status, 200, resp.result);
           chai.assert.isObject(resp.result);
-          chai.assert.equal(resp.status, 200);
+
           done();
         })
         .catch(done);
+    });
+  });
+
+  describe('download', () => {
+    it('download request is successful', (done) => {
+      dbx.sharingGetSharedLinkFile({ url: process.env.DBX_SHARED_LINK })
+        .then((resp) => {
+          chai.assert.instanceOf(resp, DropboxResponse);
+          chai.assert.equal(resp.status, 200, resp.result);
+          chai.assert.isObject(resp.result);
+
+          chai.assert.isString(resp.result.name);
+          chai.assert.isDefined(resp.result.fileBinary);
+
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  describe('upload', () => {
+    it('upload request is successful', (done) => {
+      fs.readFile(path.resolve('test/fixtures/test.txt'), 'utf8')
+        .then((contents) => {
+          dbx.filesUpload({ path: '/test.txt', contents })
+            .then((resp) => {
+              chai.assert.instanceOf(resp, DropboxResponse);
+              chai.assert.equal(resp.status, 200, resp.result);
+              chai.assert.isObject(resp.result);
+
+              done();
+            })
+            .catch(done);
+        });
     });
   });
 });
