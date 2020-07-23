80efeecfbc8fd6c15c808df51c9455473948e170
Andrew Lawson <alawson@dropbox.com>
2020-07-23 13:50:05 -0700

    Add team integration tests.

---
+++ test/integration/team.js
@@ -0,0 +1,39 @@
+import chai from 'chai';
+
+import Dropbox from '../../index.js';
+import { DropboxResponse } from '../../src/response.js';
+
+describe('Team', () => {
+  const dbx = new Dropbox({ accessToken: process.env.DROPBOX_TEAM_TOKEN });
+
+  describe('rpc', () => {
+    it('rpc request is successful', (done) => {
+      dbx.teamGetInfo()
+        .then((resp) => {
+          chai.assert.instanceOf(resp, DropboxResponse);
+          chai.assert.equal(resp.status, 200, resp.result);
+          chai.assert.isObject(resp.result);
+
+          done();
+        })
+        .catch(done);
+    });
+
+    it('select-user rpc request is successful', (done) => {
+      const dbxSelectUser = new Dropbox({
+        accessToken: process.env.DROPBOX_TEAM_TOKEN,
+        selectUser: process.env.DROPBOX_USER_ID,
+      });
+
+      dbxSelectUser.userGetCurrentAccount()
+        .then((resp) => {
+          chai.assert.instanceOf(resp, DropboxResponse);
+          chai.assert.equal(resp.status, 200, resp.result);
+          chai.assert.isObject(resp.result);
+
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
